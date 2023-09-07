d800fc236195febba3d8da02f1146e8fff69f4ef
ParthaMalakar <parthamalakar4321@gmail.com>
2023-09-08 00:05:26 +0600

    Interfaces created in DAL

---
+++ DAL/Interfaces/IEmpRepo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IEmpRepo<CLASS, ID>
+    {
+        void Update(CLASS obj);
+        CLASS Get3rdHighSelary();
+        List<CLASS> GetAllEmp();
+        List<CLASS> GetMontlySalaryEmp();
+
+        CLASS GetSupervisor(ID id);
+
+
+    }
+}
